7bd56c95cf43e4680627b001023e6f3660a62295
Robin HILAIRE <hilairerob84@gmail.com>
2024-02-12 21:48:35 +0100

    Modifie la structure de count

--- src/components/Toolbar/SlidesNav/SlidesNav.jsx
+++ src/components/Toolbar/SlidesNav/SlidesNav.jsx
@@ -1,51 +1,23 @@
-import { useState } from 'react';
 import NavButton from './NavButton.jsx';
 import FontAwesomeIcon from '../../FontAwesomeIcon/FontAwesomeIcon.jsx';
+import { useSlidesCount } from '../../../contexts/SlidesCount.context.jsx';
 
-function SlidesNav({ maxSlide=1 }) {
-    const [count, setCount] = useState(1);
+function SlidesNav() {
+    const { count, handleChange, handleDecrement, handleIncrement, handleFirst, handleLast } = useSlidesCount();
 
-    const handleChange = (e) => {
-        const value = parseInt(e.target.value);
-        if (!isNaN(value)) {
-            if (value > maxSlide)
-                setCount(maxSlide);
-            else if (value < 1)
-                setCount(1);
-            else
-                setCount(value);
-        }
-    };
-  
-    const handleDecrement = () => {
-        setCount(count-1<1?maxSlide:count-1);
-    };
-  
-    const handleIncrement = () => {
-        setCount(count+1>maxSlide?maxSlide:count+1);
-    };
-
-    const handleFirst = () => {
-        setCount(1);
-    };
-
-    const handleLast = () => {
-        setCount(maxSlide);
-    };
-  
     return (
         <div className="flex bg-white rounded-lg w-64">
-            <NavButton onNavButtonClick={() => handleFirst()}>
+            <NavButton onNavButtonClick={handleFirst}>
                 <FontAwesomeIcon icon={"fa-backward-fast"} />
             </NavButton>
-            <NavButton onNavButtonClick={() => handleDecrement()}>
+            <NavButton onNavButtonClick={handleDecrement}>
                 <FontAwesomeIcon icon={"fa-caret-left"} />
             </NavButton>
-            <input className="w-1/3 text-center bg-white font-semibold text-md text-gray-700 transition ease-in-out delay-50 hover:text-gray-800 hover:bg-gray-300 rounded-lg outline-none focus:outline-none" name="custom-input-number" value={count} onChange={handleChange} />
-            <NavButton onNavButtonClick={() => handleIncrement()}>
+            <input className="w-1/3 text-center bg-white font-semibold text-md text-gray-700 transition ease-in-out delay-50 hover:text-gray-800 hover:bg-gray-300 rounded-lg outline-none focus:outline-none" value={count} onChange={(e) => handleChange(parseInt(e.target.value))} />
+            <NavButton onNavButtonClick={handleIncrement}>
                 <FontAwesomeIcon icon={"fa-caret-right"} />
             </NavButton>
-            <NavButton onNavButtonClick={() => handleLast()}>
+            <NavButton onNavButtonClick={handleLast}>
                 <FontAwesomeIcon icon={"fa-forward-fast"} />
             </NavButton>
         </div>
---
+++ src/contexts/SlidesCount.context.jsx
@@ -0,0 +1,56 @@
+import { createContext, useContext, useState } from 'react';
+
+const SlidesCountContext = createContext();
+
+function useSlidesCount() {
+    return useContext(SlidesCountContext);
+}
+
+function SlidesCountProvider({ children, maxSlide }) {
+    const [count, setCount] = useState(1);
+
+    const handleChange = (value) => {
+        if (!isNaN(value)) {
+            if (value>maxSlide)
+                setCount(maxSlide);
+            else if (value<1)
+                setCount(1);
+            else
+                setCount(value);
+        }
+    };
+
+    const handleDecrement = () => {
+        setCount(count-1<1?maxSlide:count-1);
+    };
+
+    const handleIncrement = () => {
+        setCount(count+1>maxSlide?maxSlide:count+1);
+    };
+
+    const handleFirst = () => {
+        setCount(1);
+    };
+
+    const handleLast = () => {
+        setCount(maxSlide);
+    };
+
+    return (
+        <SlidesCountContext.Provider
+            value={{
+                count,
+                handleChange,
+                handleDecrement,
+                handleIncrement,
+                handleFirst,
+                handleLast,
+                maxSlide
+            }}
+        >
+            {children}
+        </SlidesCountContext.Provider>
+    );
+}
+
+export { useSlidesCount, SlidesCountProvider };
