54308f3c58f15f69e22a18732b9a833b3ff2f37d
Robin HILAIRE <hilairerob84@gmail.com>
2024-02-13 11:43:35 +0100

    Ajoute les numéros de diapo

--- src/components/SlideViewer/SlideViewer.jsx
+++ src/components/SlideViewer/SlideViewer.jsx
@@ -1,8 +1,19 @@
+import { useShowSlidesNumber } from "../../contexts/ShowSlidesNumber.context";
+import { useSlidesCount } from "../../contexts/SlidesCount.context";
+
 function SlideViewer({ children }) {
+    const { count, maxSlide } = useSlidesCount();
+    const { isVisible } = useShowSlidesNumber();
+
     return (
         <div className="flex justify-center items-center h-full">
-            <div className="bg-white p-2 shadow-2xl rounded-xl h-full w-full border m-4">
+            <div className="bg-white p-4 shadow-2xl rounded-xl h-full w-full border m-4">
                 {children}
+                {isVisible && (
+                    <div className="absolute bottom-0 left-1/2 -translate-x-1/2 mb-7 text-gray-600">
+                        <span className="text-md">{count}/{maxSlide}</span>
+                    </div>
+                )}
             </div>
         </div>
     );
