ada3e45e9a86f3621025e4c29568a9c8bcc59819
Robin HILAIRE <hilairerob84@gmail.com>
2024-02-13 13:49:03 +0100

    Modifie du css

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,37 @@ import Toolbar from './components/Toolbar/Toolbar.jsx';
 import SlideViewer from './components/SlideViewer/SlideViewer.jsx'
 import { useSlidesCount } from './contexts/SlidesCount.context.jsx';
 import { ShowSlidesNumberProvider } from './contexts/ShowSlidesNumber.context.jsx';
+import { useEffect } from 'react';
 
 function App() {
-    const {setMaxSlide} = useSlidesCount();
+    const {setMaxSlide, handleDecrement, handleIncrement, handleFirst, handleLast} = useSlidesCount();
     setMaxSlide(9);
+
+    useEffect(() => {
+        function handleKeyPress(event) {
+            if (event.keyCode === 39) { // Flèche droite
+                handleIncrement();
+            } else if (event.keyCode === 37) { // Flèche gauche
+                handleDecrement();
+            } else if (event.keyCode === 38) { // Flèche haut
+                handleLast();
+            } else if (event.keyCode === 40) { // Flèche bas
+                handleFirst();
+            }
+        }
+
+        window.addEventListener('keydown', handleKeyPress);
+    
+        return () => {
+            window.removeEventListener('keydown', handleKeyPress);
+        };
+    }, []);
 
     return (
         <ShowSlidesNumberProvider>
             <div className="flex flex-col h-screen">
                 <Toolbar />
-                <div className="flex-grow pb-4">
+                <div className="flex-grow">
                     <SlideViewer>
                         <div className="text-center">
                             <h2 className="text-3xl font-bold mb-4">Bienvenue sur notre plateforme de présentation</h2>
--- src/components/Toolbar/SlidesNav/NavButton.jsx
+++ src/components/Toolbar/SlidesNav/NavButton.jsx
@@ -1,6 +1,6 @@
 function NavButton({ onNavButtonClick, children }) {
     return (
-        <button onClick={onNavButtonClick} className="w-1/3 text-gray-600 transition ease-in-out delay-50 hover:text-gray-700 hover:bg-gray-300 h-full rounded-lg flex items-center justify-center focus:outline-none">
+        <button onClick={onNavButtonClick} className="w-1/3 text-gray-600 transition ease-in-out delay-50 hover:text-gray-700 hover:bg-gray-300 h-full rounded-lg flex items-center justify-center">
             {children} 
         </button>
     );
