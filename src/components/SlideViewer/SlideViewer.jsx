2bd948fa360c4915682dbd319f9f00a78871f555
RobinH <hilairerob84@gmail.com>
2024-02-14 15:34:33 +0100

    Modifie un nom de composant

--- src/components/SlideViewer/SlideViewer.jsx
+++ src/components/SlideViewer/SlideViewer.jsx
@@ -6,14 +6,16 @@ function SlideViewer({ children }) {
     const { isVisible } = useShowSlidesNumber();
 
     return (
-        <div className="flex items-center h-full pb-4">
-            <div className="bg-white p-4 shadow-2xl rounded-xl h-full w-full border m-4">
-                {children}
-                {isVisible && (
-                    <div className="absolute bottom-0 left-1/2 -translate-x-1/2 mb-7 text-gray-600">
-                        <span className="text-md">{count}/{maxSlide}</span>
-                    </div>
-                )}
+        <div className="flex-grow">
+            <div className="flex items-center h-full pb-4">
+                <div className="bg-white p-4 shadow-2xl rounded-xl h-full w-full border m-4">
+                    {children}
+                    {isVisible && (
+                        <div className="absolute bottom-0 left-1/2 -translate-x-1/2 mb-7 text-gray-600">
+                            <span className="text-md">{count}/{maxSlide}</span>
+                        </div>
+                    )}
+                </div>
             </div>
         </div>
     );
--- src/components/Toolbar/Toolbar.jsx
+++ src/components/Toolbar/Toolbar.jsx
@@ -1,12 +1,12 @@
 import SlidesNav from './SlidesNav/SlidesNav.jsx';
-import ShowButton from './ShowButton/ShowButton.jsx';
+import ShowSlidesNumberButton from './ShowSlidesNumberButton/ShowSlidesNumberButton.jsx';
 
 function Toolbar() {
     return (
         <div className="bg-white shadow-lg rounded-full p-3 m-4 flex justify-between items-center relative">
             <h1 className="text-lg font-semibold text-gray-800">Slides</h1>
             <SlidesNav />
-            <ShowButton />
+            <ShowSlidesNumberButton />
         </div>
     );
 }
