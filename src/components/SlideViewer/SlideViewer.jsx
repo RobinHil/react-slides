a12b3e246f5c8cbc59d7ad09bbd900cbfcff61ae
Robin HILAIRE <hilairerob84@gmail.com>
2024-02-14 16:49:23 +0100

    Ajoute le plein écran

--- src/components/SlideViewer/SlideViewer.jsx
+++ src/components/SlideViewer/SlideViewer.jsx
@@ -1,17 +1,19 @@
 import { useShowSlidesNumber } from "../../contexts/ShowSlidesNumber.context";
 import { useSlidesCount } from "../../contexts/SlidesCount.context";
+import { useFullScreen } from "../../contexts/FullScreen.context";
 
 function SlideViewer({ children }) {
     const { count, maxSlide } = useSlidesCount();
     const { isVisible } = useShowSlidesNumber();
+    const { isFullScreen } = useFullScreen();
 
     return (
-        <div className="flex-grow">
+        <div className={`flex-grow ${isFullScreen ? 'fixed inset-0' : ''}`}>
             <div className="flex items-center h-full pb-4">
-                <div className="bg-white p-4 shadow-2xl rounded-xl h-full w-full border m-4">
+                <div className={`bg-white h-full w-full ${isFullScreen ? 'pt-4' : 'shadow-2xl p-4 m-4 border rounded-xl'}`}>
                     {children}
                     {isVisible && (
-                        <div className="absolute bottom-0 left-1/2 -translate-x-1/2 mb-7 text-gray-600">
+                        <div className={`absolute bottom-0 left-1/2 -translate-x-1/2 text-gray-600 ${isFullScreen ? 'mb-4' : 'mb-7'}`}>
                             <span className="text-md">{count}/{maxSlide}</span>
                         </div>
                     )}
---
+++ src/contexts/FullScreen.context.jsx
@@ -0,0 +1,28 @@
+import React, { createContext, useContext, useState } from 'react';
+
+const FullScreenContext = createContext(null);
+
+function useFullScreen() {
+    return useContext(FullScreenContext);
+}
+
+function FullScreenProvider({ children }) {
+    const [isFullScreen, setFullScreen] = useState(false);
+
+    const toggleFullScreen = () => {
+        setFullScreen(!isFullScreen);
+    };
+
+    return (
+        <FullScreenContext.Provider
+            value={{
+                isFullScreen,
+                toggleFullScreen
+            }}
+        >
+            {children}
+        </FullScreenContext.Provider>
+    );
+}
+
+export { useFullScreen, FullScreenProvider };
