a1784ea48ca06f99f734f6313eae5444886d15a3
RobinH <hilairerob84@gmail.com>
2024-02-16 11:35:10 +0100

    Modifie les styles des paragraphes

--- src/components/slides/content/Paragraph.jsx
+++ src/components/slides/content/Paragraph.jsx
@@ -1,5 +1,9 @@
-function Paragraph({ children, fontSize="md" }) {
-    const combinedStyle = `text-black text-${fontSize}`;
+import classNames from "classnames";
+import { textSize, textAlign, textColor, bgColor } from "./theme.js";
+
+function Paragraph({ children, fontSize=3, align="left", color="black", bg="transparent"}) {
+    const combinedStyle = classNames(textSize[fontSize], textAlign[align], textColor[color], bgColor[bg]);
+
     return (
         <p className={combinedStyle}>{children}</p>
     );
