a5bd4a541674ec8e9f20d5abbb4f6f1257cd044c
RobinH <hilairerob84@gmail.com>
2024-02-29 16:46:54 +0100

    Modifie les Image

--- src/slideComponents/Image.jsx
+++ src/slideComponents/Image.jsx
@@ -1,19 +1,19 @@
-import { flexAlign, bgColor, textSize, textColor } from './theme.js';
+import { flexAlign, bgColor, textSize, textColor, imgWidth } from './theme.js';
 
-function Image({ children, src = "", alt = "", align = "left", fontSize = 2, fontColor = "black", bg = "transparent" }) {
+function Image({ children, src="", alt="", align="left", fontSize=2, fontColor="black", bg="transparent", width="auto" }) {
     const combinedStyle = `flex flex-col ${flexAlign[align]} ${bgColor[bg]} ${textSize[fontSize]} ${textColor[fontColor]}`;
 
     return (
         <div className={combinedStyle}>
             {children?(
                 <figure>
-                    <img src={src} alt={alt} />
+                    <img src={src} alt={alt} className={`h-auto ${imgWidth[width]}`} />
                     <figcaption className="italic text-center">
                         {children}
                     </figcaption>
                 </figure>
             ):(
-                <img src={src} alt={alt} />
+                <img src={src} alt={alt} className={`h-auto ${imgWidth[width]}`} />
             )}
         </div>
     );
