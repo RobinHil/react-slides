bed20e955c046631bb1e7ec6cb6990e426a148e4
Robin HILAIRE <hilairerob84@gmail.com>
2024-02-29 16:55:58 +0100

    Modifie les composants

--- src/slideComponents/Heading.jsx
+++ src/slideComponents/Heading.jsx
@@ -1,7 +1,7 @@
 import { bgColor, headingSize, textAlign, textColor } from './theme.js';
 
-function Heading({ children, fontSize=6, align="left", color="black", bg="transparent" }) {
-    const combinedStyle = `font-bold ${headingSize[fontSize]} ${textAlign[align]} ${textColor[color]} ${bgColor[bg]}`;
+function Heading({ children, fontSize=6, align="left", color="black", bg="transparent", italic=false, underline=false, crossed=false }) {
+    const combinedStyle = `font-bold ${italic?"italic":""} ${crossed?"line-through":""} ${underline?"underline":""} ${headingSize[fontSize]} ${textAlign[align]} ${textColor[color]} ${bgColor[bg]}`;
 
     return (
         <h1 className={combinedStyle}>{children}</h1>
