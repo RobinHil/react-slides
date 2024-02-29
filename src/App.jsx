a1b0d4da0fc72560a81608ad11be667ca836aa33
Robin HILAIRE <hilairerob84@gmail.com>
2024-02-29 14:01:58 +0100

    Modifie App

--- src/App.jsx
+++ src/App.jsx
@@ -2,37 +2,25 @@ import Toolbar from './interface/Toolbar.jsx';
 import SlideViewer from './interface/SlideViewer.jsx';
 import { SlidesCountProvider, useSlidesCount } from './contexts/SlidesCount.context.jsx';
 import ExitFullScreenButton from './interface/ExitFullScreenButton.jsx';
-import { FullScreenProvider } from './contexts/FullScreen.context.jsx';
 import SlideLoader from './interface/SlideLoader.jsx';
 import slides from './slides/index.jsx';
 import SlideMosaic from './interface/SlideMosaic.jsx';
-import { MosaicProvider } from './contexts/Mosaic.context.jsx';
 import { useMosaic } from './contexts/Mosaic.context.jsx';
 
 function App() {
-  return (
-    <SlidesCountProvider max={slides.length}>
-      <FullScreenProvider>
-        <MosaicProvider>
-          <AppContent />
-        </MosaicProvider>
-      </FullScreenProvider>
-    </SlidesCountProvider>
-  );
-}
-
-function AppContent() {
   const { isVisible } = useMosaic();
 
   return (
-    <div className={`flex flex-col h-screen ${isVisible ? 'ml-32' : ''}`}>
-      <Toolbar />
-      <SlideViewer>
-        <SlideLoader slides={slides} />
-      </SlideViewer>
-      <ExitFullScreenButton />
-      <SlideMosaic />
-    </div>
+    <SlidesCountProvider max={slides.length}>
+      <div className={`flex flex-col h-screen ${isVisible ? 'ml-32' : ''}`}>
+        <Toolbar />
+        <SlideViewer>
+          <SlideLoader slides={slides} />
+        </SlideViewer>
+        <ExitFullScreenButton />
+        <SlideMosaic />
+      </div>
+    </SlidesCountProvider>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+import { MosaicProvider } from './contexts/Mosaic.context.jsx';
+import { FullScreenProvider } from './contexts/FullScreen.context.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
+        <FullScreenProvider>
+        <MosaicProvider>
             <App />
+        </MosaicProvider>
+        </FullScreenProvider>
     </React.StrictMode>,
 );
