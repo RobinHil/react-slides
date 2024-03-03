49c509a014109f7a35dbcfcdab9652e08a320b58
Robin HILAIRE <hilairerob84@gmail.com>
2024-03-03 16:26:25 +0100

    Modifie la structure du projet

--- src/slides/index.jsx
+++ src/slides/index.jsx
@@ -1,13 +1,7 @@
-import Slide1 from './Slide1.jsx';
-import Slide2 from './Slide2.jsx';
-import Slide3 from './Slide3.jsx';
-import Slide4 from './Slide4.jsx';
+import Title from './Title.jsx';
 
 const slides = [
-    Slide1,
-    Slide2,
-    Slide3,
-    Slide4
+    Title
 ];
 
 export default slides;
