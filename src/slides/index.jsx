e72a1866ffd85a3a2f8bf76ac84f0d160bed00d3
Robin HILAIRE <hilairerob84@gmail.com>
2024-03-03 19:31:16 +0100

    Ajoute la page des textes bis

--- src/slides/index.jsx
+++ src/slides/index.jsx
@@ -1,11 +1,12 @@
 import Title from './Title.jsx';
 import Summary from './Summary.jsx';
-import Texts from './Texts.jsx';
+import { Texts1, Texts2 } from './Texts.jsx';
 
 const slides = [
     Title,
     Summary,
-    Texts
+    Texts1,
+    Texts2
 ];
 
 export default slides;
