c4728f2e4337967b56ac7699cf06b867adfe4f06
Robin HILAIRE <hilairerob84@gmail.com>
2024-03-03 15:59:04 +0100

    Modifie des styles

--- src/interface/Mosaic.jsx
+++ src/interface/Mosaic.jsx
@@ -8,11 +8,11 @@ function Mosaic() {
     const { isFullScreen } = useFullScreen();
 
     return (
-        <div className={`fixed top-0 left-0 bottom-0 w-36 text-white p-4 flex flex-col overflow-auto transition duration-300 ${isVisible&&!isFullScreen?"translate-x-0":"-translate-x-full"}`}>
+        <div className={`fixed top-0 left-0 bottom-0 w-36 text-black p-4 flex flex-col overflow-auto transition duration-300 ${isVisible&&!isFullScreen?"translate-x-0":"-translate-x-full"}`}>
             {Array.from({ length: state.maxSlide }, (_, index) => (
                 <button
                     key={index+1}
-                    className={`w-full h-14 min-h-12 flex items-center justify-center mb-2 font-semibold rounded-lg outline-none ${state.count===index+1?"bg-gray-400":"bg-gray-600 transition ease-in-out delay-50 hover:bg-gray-500"}`}
+                    className={`w-full h-16 min-h-14 flex items-center justify-center mb-2 font-semibold rounded-lg outline-none bg-gray-100 transition ease-in-out delay-50 hover:bg-gray-200 border-2 duration-200 ${state.count===index+1?"border-cyan-600":"border-gray-300"}`}
                     onClick={() => dispatch({ type: "change", payload: index+1 })}
                 >
                     {index+1}
