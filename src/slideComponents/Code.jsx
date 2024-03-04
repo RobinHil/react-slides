bec317b1c1b3c302aa1ddf005ce1acf36898fa70
RobinH <hilairerob84@gmail.com>
2024-03-04 18:03:20 +0100

    Ajoute la slide de code

--- src/slideComponents/Code.jsx
+++ src/slideComponents/Code.jsx
@@ -1,11 +1,14 @@
-import { Prism } from 'react-syntax-highlighter';
-import { vscDarkPlus, vs } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { vs, vs2015 } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import { textSize } from './theme.js';
 
-function Code({ children, language="", theme="" }) {
+function Code({ children, language="", theme="", fontSize=4 }) {
+    const combinedStyle = `rounded-md ${textSize[fontSize]} ${theme==="light"?"border-2 border-gray-200":"border-none"}`
+
     return (
-        <Prism language={language} style={theme==="light"?vs:vscDarkPlus} className="rounded-md">
+        <SyntaxHighlighter language={language} style={theme==="light"?vs:vs2015} className={combinedStyle}>
             {children}
-        </Prism>
+        </SyntaxHighlighter>
     );
 };
 
--- src/slides/Code_area.jsx
+++ src/slides/Code_area.jsx
@@ -1,9 +1,67 @@
-import { CenterLayout, Heading } from '../slideComponents/index.jsx';
+import { CenterLayout, Code, Heading, Paragraph, TwoColumnLayout } from '../slideComponents/index.jsx';
 
 function Code_area() {
     return (
         <CenterLayout>
             <Heading align="center" fontSize={8}>Code</Heading>
+            <br /><br />
+            <TwoColumnLayout
+                leftContent={<CenterLayout>
+                    <Heading align="center">Thème sombre</Heading>
+                    <br />
+                    <Code language="cpp">{
+`#include <iostream>
+using namespace std;
+
+template <typename T>
+T moyenne(T tableau[], int taille) {
+    T somme = 0;
+    for (int i = 0; i < taille; i++) {
+        somme += tableau[i];
+    }
+    return somme / taille;
+}
+
+int main() {
+    int tableauEntiers[] = {1, 2, 3, 4, 5};
+    double tableauReels[] = {1.1, 2.2, 3.3, 4.4, 5.5};
+
+    cout << "Moyenne des entiers : " << moyenne(tableauEntiers, 5) << endl;
+    cout << "Moyenne des réels : " << moyenne(tableauReels, 5) << endl;
+
+    return 0;
+}`
+                    }</Code>
+                </CenterLayout>}
+
+                rightContent={<CenterLayout>
+                    <Heading align="center">Thème clair</Heading>
+                    <br />
+                    <Code theme="light" language="cpp">{
+`#include <iostream>
+using namespace std;
+
+template <typename T>
+T moyenne(T tableau[], int taille) {
+    T somme = 0;
+    for (int i = 0; i < taille; i++) {
+        somme += tableau[i];
+    }
+    return somme / taille;
+}
+
+int main() {
+    int tableauEntiers[] = {1, 2, 3, 4, 5};
+    double tableauReels[] = {1.1, 2.2, 3.3, 4.4, 5.5};
+
+    cout << "Moyenne des entiers : " << moyenne(tableauEntiers, 5) << endl;
+    cout << "Moyenne des réels : " << moyenne(tableauReels, 5) << endl;
+
+    return 0;
+}`
+                    }</Code>
+                </CenterLayout>}
+            />
         </CenterLayout>
     );
 }
