377cf31b6589a835d056854619f9e3c3e9cada40
RobinH <hilairerob84@gmail.com>
2024-03-04 19:35:33 +0100

    Ajoute une première version de la slide de Markdown

--- src/slideComponents/Markdown.jsx
+++ src/slideComponents/Markdown.jsx
@@ -1,28 +1,49 @@
+import React from 'react';
 import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+import rehypeRaw from 'rehype-raw';
+import { textSize, listType } from './theme.js';
 import Paragraph from './Paragraph.jsx';
 import Heading from './Heading.jsx';
 import Link from './Link.jsx';
 import Code from './Code.jsx';
 
+function isCheckbox(children) {
+    if (React.isValidElement(children)) {
+        const { type, props } = children;
+        if (type==="input"&&props.type==="checkbox")
+            return true;
+        if (props.children)
+            return isCheckbox(props.children);
+    }
+  
+    if (Array.isArray(children))
+        for (const child of children)
+            if (isCheckbox(child))
+                return true;
+  
+    return false;
+}
+
 function Markdown({ children }) {
     const components = {
-        p: ({ children }) => <Paragraph>{children}</Paragraph>,
+        p: ({ children }) => <Paragraph fontSize={6}>{children}</Paragraph>,
         h1: ({ children }) => <Heading fontSize={9}>{children}</Heading>,
         h2: ({ children }) => <Heading fontSize={8}>{children}</Heading>,
         h3: ({ children }) => <Heading fontSize={7}>{children}</Heading>,
         h4: ({ children }) => <Heading fontSize={6}>{children}</Heading>,
         h5: ({ children }) => <Heading fontSize={5}>{children}</Heading>,
         h6: ({ children }) => <Heading fontSize={4}>{children}</Heading>,
-        ul: ({ children }) => <ul className={"list-inside list-disc text-sm text-black bg-transparent text-left"}>{children}</ul>,
-        ol: ({ children }) => <ol className={"list-inside list-decimal text-sm text-black bg-transparent text-left"}>{children}</ol>,
-        li: ({ children }) => <li key={Math.random()}>{children}</li>,
+        ul: ({ children }) => <ul className={`list-inside ${listType.bulleted} ${textSize[6]} text-black bg-transparent text-left`}>{children}</ul>,
+        ol: ({ children }) => <ol className={`list-inside ${listType.numbered} ${textSize[6]} text-black bg-transparent text-left`}>{children}</ol>,
+        li: ({ children }) => <li key={Math.random()} className={isCheckbox(children)?"list-none":""}>{children}</li>,
         blockquote: ({ children }) => <blockquote className={"border-l-4 border-gray-500 pl-4 italic my-4"}>{children}</blockquote>,
-        code: ({ children, className }) => <Code language={(/language-(\w+)/.exec(className || ''))[1]}>{children}</Code>,
-        a: ({ children, href }) => <Link href={href}>{children}</Link>
+        code: ({ children, className }) => <Code language={!className?"":(/language-(\w+)/.exec(className||""))[1]} fontSize={4}>{children}</Code>,
+        a: ({ children, href }) => <Link href={href} fontSize={6}>{children}</Link>
     };
 
     return (
-        <ReactMarkdown components={components}>
+        <ReactMarkdown components={components} remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeRaw]}>
             {children}
         </ReactMarkdown>
     );
